aa1c84be38496d78d3aceb8c11a7891ec6d1c7ac
BEQSONA-cmd <tvildiani2001@gmail.com>
2025-12-09 18:01:32 +0100

    updates

--- back/src/api/translate.ts
+++ back/src/api/translate.ts
@@ -2,8 +2,11 @@ import { FastifyInstance } from "fastify";
 import fs from "fs";
 import path from "path";
 
-const dictionaryPath = path.join(__dirname, "../../data.json");
-const dictionary = JSON.parse(fs.readFileSync(dictionaryPath, "utf8"));
+const ge_en = path.join(__dirname, "../../ge-en.json");
+const en_ge = path.join(__dirname, "../../en-ge.json");
+
+const dictionary_ge_en = JSON.parse(fs.readFileSync(ge_en, "utf8"));
+const dictionary_en_ge = JSON.parse(fs.readFileSync(en_ge, "utf8"));
 
 export default async function translateRoutes(fastify: FastifyInstance) {
     fastify.post("/api/translate", async (req, res) => {
@@ -14,7 +17,7 @@ export default async function translateRoutes(fastify: FastifyInstance) {
                 to: string;
             };
 
-            const translated = translateText(text.toLowerCase());
+            const translated = translateText(text.toLowerCase(), from, to);
 
             return res.send({ translatedText: translated });
         } catch (error) {
@@ -24,10 +27,20 @@ export default async function translateRoutes(fastify: FastifyInstance) {
     });
 }
 
-function translateText(text: string) {
+function translateText(text: string, from: string, to: string): string {
     const words = text.split(/\s+/);
 
     const translatedWords: string[] = [];
+    let dictionary: { [key: string]: string } = {};
+    console.log(from, to);
+    if (from === "Georgian" && to === "English") {
+        dictionary = dictionary_ge_en;
+    } else if (from === "English" && to === "Georgian") {
+        dictionary = dictionary_en_ge;
+    } else {
+        return text;
+    }
+
 
     for (let i = 0; i < words.length; i++) {
         let foundPhrase = false;
